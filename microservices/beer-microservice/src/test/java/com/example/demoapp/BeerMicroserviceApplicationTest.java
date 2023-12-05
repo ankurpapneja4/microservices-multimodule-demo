4c0ef926d8cff3f8f405b5f4db37f949577d8971
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-12-05 13:12:55 +0530

    initial test added. for #13

---
+++ microservices/beer-microservice/src/test/java/com/example/demoapp/BeerMicroserviceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeerMicroserviceApplicationTest {
+
+    @Test
+    void contextLoads(){ }
+}
