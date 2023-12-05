ea9818672211dc9930ce79b232ccf59a38e41a7a
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-12-05 13:06:08 +0530

    BeerController added. closes #12

---
+++ microservices/beer-microservice/src/main/java/com/example/demoapp/controller/BeerController.java
@@ -0,0 +1,41 @@
+package com.example.demoapp.controller;
+
+import com.example.demoapp.web.model.BeerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable UUID beerId){
+
+        //Todo - implementation
+        return ResponseEntity.ok( BeerDTO.builder().build() );
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer( @RequestBody BeerDTO beerDTO ){
+
+        //Todo - implementation
+        return ResponseEntity
+                    .created( URI.create( "/api/v1/beer/" + UUID.randomUUID().toString()) )
+                    .build();
+    }
+
+    @PutMapping("/{beerId}")
+    @ResponseStatus( HttpStatus.NO_CONTENT )
+    public void updateBeer( @PathVariable UUID beerId, @RequestBody BeerDTO beerDTO ){
+
+        //Todo - Implementation
+
+    }
+
+
+}
+
