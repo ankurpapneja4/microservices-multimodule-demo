27e6f73500bb124fddef139a400bf7e4c5beb4b7
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-12-06 17:18:19 +0530

    Load properties from application.yaml. @ConfigurationProperties added. for #7. closes #15

--- shared-modules/clients/src/main/java/com/example/demoapp/web/client/BeerServiceClient.java
+++ shared-modules/clients/src/main/java/com/example/demoapp/web/client/BeerServiceClient.java
@@ -1,14 +1,17 @@
 package com.example.demoapp.web.client;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 @Slf4j
+@ConfigurationProperties("microservices.beer-microservice.config")
 public class BeerServiceClient {
 
+    private String apihost;
     private final RestTemplate restTemplate;
 
     public BeerServiceClient(RestTemplateBuilder restTemplateBuilder){
