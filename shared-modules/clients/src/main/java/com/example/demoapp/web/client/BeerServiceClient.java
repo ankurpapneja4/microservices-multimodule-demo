3e5447d1e44e218cb1c1926b8ed135fe2b4e3b5d
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-12-06 17:27:45 +0530

    BeerServiceClient::getBeerById added. for #7

--- shared-modules/clients/src/main/java/com/example/demoapp/web/client/BeerServiceClient.java
+++ shared-modules/clients/src/main/java/com/example/demoapp/web/client/BeerServiceClient.java
@@ -1,10 +1,13 @@
 package com.example.demoapp.web.client;
 
+import com.example.demoapp.web.model.BeerDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Component
 @Slf4j
@@ -14,7 +17,14 @@ public class BeerServiceClient {
     private String apihost;
     private final RestTemplate restTemplate;
 
+    private final String V1_BEER_PATH = "/api/v1/beer/";
+
     public BeerServiceClient(RestTemplateBuilder restTemplateBuilder){
         restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDTO getBeerById(UUID beerId){
+
+        return restTemplate.getForObject( apihost + V1_BEER_PATH + beerId , BeerDTO.class );
     }
 }
