3f2fa4cd763296687dd1521a6b9974d5f156cd96
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-12-26 12:43:36 +0530

    jackson configuration added. closes #28

--- shared-modules/web-api/src/main/java/com/example/demoapp/web/model/BeerDTO.java
+++ shared-modules/web-api/src/main/java/com/example/demoapp/web/model/BeerDTO.java
@@ -1,6 +1,10 @@
 package com.example.demoapp.web.model;
 
-import jakarta.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,9 +25,11 @@ public class BeerDTO {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -37,7 +43,7 @@ public class BeerDTO {
 
     @Positive
     @NotNull
-    @Digits(integer = 5, fraction = 2)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
 
     private Integer quantityOnHand;
